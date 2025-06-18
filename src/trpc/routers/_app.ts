3395be5add02bed0c1f89ed1b4b14709cdab6557
iinsue <hinsu93@gmail.com>
2025-06-19 01:39:25 +0900

    #19 Checkout Page

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,6 +2,7 @@ import { authRouter } from "@/modules/auth/server/procedures";
 import { tagsRouter } from "@/modules/tags/server/procedures";
 import { tenantsRouter } from "@/modules/tenants/server/procedures";
 import { productsRouter } from "@/modules/products/server/procedures";
+import { checkoutRouter } from "@/modules/checkout/server/procedures";
 import { categoriesRouter } from "@/modules/categories/server/procedures";
 
 import { createTRPCRouter } from "../init";
@@ -10,6 +11,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   tags: tagsRouter,
   tenants: tenantsRouter,
+  checkout: checkoutRouter,
   products: productsRouter,
   categories: categoriesRouter,
 });
