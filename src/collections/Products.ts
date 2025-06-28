11154f1c2f96f087289e188a8eaf79379e1b466b
iinsue <hinsu93@gmail.com>
2025-06-28 22:08:23 +0900

    #24 Access Control

--- src/collections/Products.ts
+++ src/collections/Products.ts
@@ -1,12 +1,25 @@
 import { CollectionConfig } from "payload";
 
+import { isSuperAdmin } from "@/lib/access";
+import { Tenant } from "@/payload-types";
+
 export const Products: CollectionConfig = {
   slug: "products",
+  access: {
+    create: ({ req }) => {
+      if (isSuperAdmin(req.user)) return true;
+
+      const tenant = req.user?.tenants?.[0]?.tenant as Tenant;
+
+      return Boolean(tenant?.stripeDetailsSubmitted);
+    },
+  },
   admin: {
     useAsTitle: "name",
   },
   fields: [
     { name: "name", type: "text", required: true },
+    // TODO: Change to RichText
     { name: "description", type: "text" },
     {
       name: "price",
@@ -38,6 +51,15 @@ export const Products: CollectionConfig = {
       type: "select",
       options: ["30-day", "14-day", "7-day", "3-day", "1-day", "no-refunds"],
       defaultValue: "30-day",
+    },
+    {
+      name: "content",
+      // TODO: Change to RichText
+      type: "textarea",
+      admin: {
+        description:
+          "Protected content only visible to customers after purchase. Add product documentation, downloadable files, getting started guides, and bonus materials. Supports Markdown formatting",
+      },
     },
   ],
 };
